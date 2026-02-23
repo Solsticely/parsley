fcfdc224f939f30e42e0293ee0711ca841293608
Solsticely <96320174+Solsticely@users.noreply.github.com>
2026-02-22 16:45:52 -0800

    🎉 First commit :D stealing stuff i hope thats okay

---
+++ rust_demon/src/main.rs
@@ -0,0 +1,21 @@
+mod joystick;
+use joystick::Axis;
+
+fn main() {
+    let joystick = joystick::Joystick::new().expect("Can't acquire joystick!");
+    println!(
+        "Created joystick with device path {}",
+        joystick.device_path().expect("Can't acquire joystick!").to_string_lossy()
+    );
+    
+    loop {
+        println!("Down!");
+        joystick.move_axis(Axis::Y, -512).expect("AAA");
+        joystick.synchronise().expect("AAA");
+        std::thread::sleep(std::time::Duration::from_secs_f32(1.0));
+        println!("Up!");
+        joystick.move_axis(Axis::Y, 512).expect("AAA");
+        joystick.synchronise().expect("AAA");
+        std::thread::sleep(std::time::Duration::from_secs_f32(1.0));
+    }
+}
